61580cc305aac37bd7858c6889e6f08beace22c4
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-10 23:41:04 +0430

    remove contructor

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealScreen extends StatelessWidget {
-  final String catid;
-  final String cattitle;
+  // final String catid;
+  // final String cattitle;
 
-  CategoryMealScreen(this.catid , this.cattitle);
+  // CategoryMealScreen(this.catid , this.cattitle);
   @override
   Widget build(BuildContext context) {
+    final routeArgument =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final caattid = routeArgument['id'];
+    final cattitle = routeArgument['title'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(cattitle),
+        title: Text(cattitle!),
       ),
       body: Center(
         child: Text('this is another page'),
