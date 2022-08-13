aad85e7763608ccf194e1ef9a2838cd2218837a8
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-13 23:57:58 +0430

    add ListView of meal

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import '166 dummy_data.dart';
 
+// ignore: use_key_in_widget_constructors
 class CategoryMealScreen extends StatelessWidget {
-  // final String catid;
-  // final String cattitle;
-
-  // CategoryMealScreen(this.catid , this.cattitle);
   @override
   Widget build(BuildContext context) {
     final routeArgument =
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
-    final caattid = routeArgument['id'];
+    final catId = routeArgument['id'];
     final cattitle = routeArgument['title'];
+
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(catId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(cattitle!),
       ),
-      body: Center(
-        child: Text('this is another page'),
+      body: ListView.builder(
+        itemCount: categoryMeal.length,
+        itemBuilder: (contxt, index) {
+          return Text(categoryMeal[index].title);
+        },
       ),
     );
   }
