9b68a1bf37a2f99bdd94f933d4ab0a948446e060
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-16 00:58:06 +0430

    delete app bar

--- lib/pages/category_screen.dart
+++ lib/pages/category_screen.dart
@@ -5,20 +5,17 @@ import '../data/166 dummy_data.dart';
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('MealApp')),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: GridView(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 210,
-                childAspectRatio: 1.3,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 30),
-            children: DUMMY_CATEGORIES.map((item) {
-              return CategoryItem(item.color, item.title, item.id);
-            }).toList()),
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 210,
+              childAspectRatio: 1.3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 30),
+          children: DUMMY_CATEGORIES.map((item) {
+            return CategoryItem(item.color, item.title, item.id);
+          }).toList()),
     );
   }
 }
