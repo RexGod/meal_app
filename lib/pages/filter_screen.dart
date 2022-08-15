36b1d7391849e3ea64721d2cd5bdfe723dfbc617
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-16 02:06:41 +0430

    add filter screen

---
+++ lib/pages/filter_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routName = '/Filter';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('filters here'),
+    );
+  }
+}
