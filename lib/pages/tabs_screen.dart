a767b764a546a6fded7a10531bb559b328c9fe66
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-16 00:56:36 +0430

    add and styling bottomNavigationBar

---
+++ lib/pages/tabs_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './category_screen.dart';
+import './favorite.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _screens = [
+    {'pages': CategoryScreen(), 'title': 'Categotries'},
+    {'pages': FavoriteScreen(), 'title': 'You\'r Favorits'}
+  ];
+  int _selectPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar:
+            AppBar(title: Text(_screens[_selectPageIndex]['title'] as String)),
+        body: _screens[_selectPageIndex]['pages'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+            onTap: _selectPage,
+            backgroundColor: Theme.of(context).primaryColor,
+            // ignore: deprecated_member_use
+            selectedItemColor: Theme.of(context).accentColor,
+            unselectedItemColor: Colors.white,
+            currentIndex: _selectPageIndex,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.category), label: 'Categories'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.star), label: 'Favoriets')
+            ]));
+  }
+}
