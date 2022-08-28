3e9a6f6668d42a9ffa11db9cd8c9b246a66fa176
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-28 12:00:10 +0430

    fixed filter screen

--- lib/pages/filter_screen.dart
+++ lib/pages/filter_screen.dart
@@ -1,12 +1,19 @@
+import '../widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class FiltersScreen extends StatelessWidget {
-  static const routName = '/Filter';
-
+  static const routName = '/filter';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('filters here'),
+      appBar: AppBar(
+        title: const Text('filter page'),
+      ),
+      drawer: MainDrawer(),
+      body: const Center(
+        child: Text('filter'),
+      ),
     );
   }
 }
