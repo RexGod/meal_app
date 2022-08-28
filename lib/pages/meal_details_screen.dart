b6bed42d3b730523f9a411d3d1c8194485ed21b4
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-28 13:38:58 +0430

    add floating action button

--- lib/pages/meal_details_screen.dart
+++ lib/pages/meal_details_screen.dart
@@ -34,52 +34,59 @@ class MealDetailsScreen extends StatelessWidget {
     final selectedMeal =
         DUMMY_MEALS.firstWhere((element) => mealId == element.id);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(selectedMeal.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                selectedMeal.imageUrl,
-                fit: BoxFit.cover,
+        appBar: AppBar(
+          title: Text(selectedMeal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            selectSection(context, 'Ingredients'),
-            buildContainer(
-              ListView.builder(
-                itemBuilder: (context, index) => Card(
-                  color: Theme.of(context).accentColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    child: Text(
-                      selectedMeal.ingredients[index],
+              selectSection(context, 'Ingredients'),
+              buildContainer(
+                ListView.builder(
+                  itemBuilder: (context, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(
+                        selectedMeal.ingredients[index],
+                      ),
                     ),
                   ),
+                  itemCount: selectedMeal.ingredients.length,
                 ),
-                itemCount: selectedMeal.ingredients.length,
               ),
-            ),
-            selectSection(context, 'Steps'),
-            buildContainer(ListView.builder(
-              itemBuilder: (context, index) => Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(child: Text('#${index + 1}')),
-                    title: Text(selectedMeal.steps[index]),
-                  ),
-                  const Divider(),
-                ],
-              ),
-              itemCount: selectedMeal.steps.length,
-            )),
-          ],
+              selectSection(context, 'Steps'),
+              buildContainer(ListView.builder(
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(child: Text('#${index + 1}')),
+                      title: Text(selectedMeal.steps[index]),
+                    ),
+                    const Divider(),
+                  ],
+                ),
+                itemCount: selectedMeal.steps.length,
+              )),
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).pop(mealId);
+          },
+          child: const Icon(
+            Icons.delete,
+          ),
+        ));
   }
 }
