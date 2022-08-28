37f85ec9ddeef331013ed1b6f9df72ce53aa02fc
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-28 11:59:09 +0430

    change text to center

--- lib/pages/favorite.dart
+++ lib/pages/favorite.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+
 // ignore: use_key_in_widget_constructors
 class FavoriteScreen extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return const Center(
       child: Text('its favorite page'),
     );
   }
