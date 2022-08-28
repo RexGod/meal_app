3ddad663ffb1e2eafe5ac6496b6f94c025b7f30b
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-28 13:40:51 +0430

    change to stateful

--- lib/pages/filter_screen.dart
+++ lib/pages/filter_screen.dart
@@ -2,8 +2,14 @@ import '../widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routName = '/filter';
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
