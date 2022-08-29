a9871ae179e314f0a2afec449beb64a65124adca
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-29 15:48:07 +0430

    get  list of meal from main file and add initsatate

--- lib/pages/tabs_screen.dart
+++ lib/pages/tabs_screen.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/model/meal.dart';
 import './category_screen.dart';
 import './favorite.dart';
 import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
+  List<Meal> favoriteMeals;
+  TabsScreen(this.favoriteMeals);
+
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Map<String, Object>> _screens = [
-    {'pages': CategoryScreen(), 'title': 'Categotries'},
-    {'pages': FavoriteScreen(), 'title': 'You\'r Favorits'}
-  ];
+  late final List<Map<String, Object>> _screens;
   int _selectPageIndex = 0;
+ @override
+  void initState() {
+    _screens = [
+    {'pages': CategoryScreen(), 'title': 'Categotries'},
+    {'pages': FavoriteScreen(widget.favoriteMeals), 'title': 'You\'r Favorits'}
+  ];
+    super.initState();
+  }
   void _selectPage(int index) {
     setState(() {
       _selectPageIndex = index;
