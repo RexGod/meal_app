c5a9d02808be741e3954ce940a50bebc62ad2636
shadmehr <shadmehrmanoochehri@gmial.com>
2022-08-29 15:46:41 +0430

    add mealItems to favorite page

--- lib/pages/favorite.dart
+++ lib/pages/favorite.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../model/meal.dart';
+import '../widgets/meal_item.dart';
+
 // ignore: use_key_in_widget_constructors
 class FavoriteScreen extends StatelessWidget {
+  List<Meal> favoriteMeals;
+  FavoriteScreen(this.favoriteMeals);
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    if(favoriteMeals.isEmpty){return const Center(
       child: Text('its favorite page'),
-    );
+    );}
+    else{
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (contxt, index) {
+          return MealItems(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            
+          );
+        },
+      );
+    }
   }
 }
